579f29a495ae4414340c57288549d417823b22cd
Nathan <pi.gutierrezz@duocuc.cl>
2025-05-22 20:51:19 -0400

    SUMA

--- Calculadora 6.5.py
+++ Calculadora 6.5.py
@@ -1,15 +1,12 @@
 num1 = int(input("numero 1: ")) 
 num2 = int(input("numero 2: "))
-num3 = int(input("numero 3: ")) 
+
 
 valor = 0
 while True:
     print("""seleccione opcion
             1- Sumar 
-            2- Restar
-            3- Multiplicar
-            4- dividir 
-            5- sumar 3 valores
+            2- Salir
         """)
 
     valor = int(input("Elige una opcion: ") )
@@ -18,17 +15,9 @@ while True:
         print("la suma es",num1+num2)
         break;
     if valor == 2:
-        print("la resta es",num1-num2)
+        print(" SALIR ")
         break;
-    if valor == 3:
-        print("la multiplicacion es",num1*num2)
-        break;
-    if valor == 4:
-        print("la division es",num1/num2)
-        break;
-    if valor == 5:
-        print("la suma es",num1+num2+num3)
-        break;
+    
     else:
         print("Opcion incorrecta")
         break;
